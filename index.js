38a2f873cd500c456631b7efe1e38b9cf756ef5e
ejames9 <foster9@icloud.com>
2016-03-23 01:10:12 -0700

    Various fixes before 1.0 release.

--- index.js
+++ index.js
@@ -18,98 +18,159 @@ URL: ""
 //DONE:0 Complete X-Browser 'style' functions, and implement X-Browser compatibility in EventListener functions.
 
 
-var element = require('./lib/Element'),
-   requests = require('./lib/httpRequests'),
-     events = require('./lib/events'),
-     consol = require('./lib/logger'),
-      utils = require('./lib/utilities'),
-        dom = require('./lib/DOM'),
-              require('babel-polyfill');
+import Element from './lib/Element';
+import * as requests from './lib/httpRequests';
+import * as events from './lib/events';
+import * as consol from './lib/logger';
+import * as utils from './lib/utilities';
+import * as DOM from './lib/DOM';
+
+
+// require('babel-polyfill');
 
 
 
+
+var element = function(el) {
+  return new Element();
+};
 
 /*This function copies the prototype object of a superConstructor to the prototype object
 of a constructor. It functions just like nodes' util.inherits function, it copies methods only,
 not internal properties.*/
-var proto = (construct, superConstruct)=> {
+var proto = function(construct, superConstruct) {
   return utils.proto(construct, superConstruct);
 };
 
 
 /*This convenience function sets imported module function names as global variables, so that
 the module variable doesn't need to preface every function.*/
-var functions = function(funcs, module) {                                      //TEST:40 Test this with other node_modules.
-  return misc.functions(funcs, module);
+var functions = function(funcs, mod) {                                      //TEST:110 Test this with other node_modules.
+  var context = mod || this;
+
+  if (Array.isArray(funcs)) {
+    log(mod, 'red');
+
+    funcs.forEach(function(func) {
+      var evil = new Function('window.' + func + ' = ' + 'this.' + func + ';');
+          evil.call(context);
+    }, context);
+  } else {
+    (function() {
+      var evil = new Function('window.' + funcs + ' = ' + 'this.' + funcs + ';');
+          evil.call(context);
+    }).apply(context);
+  }
 };
 
 
-/*This function queries the dom, getting one element for each query (i.e. class/tags), creates
-an Element object with it and returns it, so that it may be easily styled.*/
-var css = function(l) {
-  return dom.css(l);
+var _$ = function(el) {
+  var l = (null !== utils.queryDOM(el));
+  return l;
 };
+
+//This function queries the dom, getting one element for each query (i.e. class/tags), creates
+//an Element object with it and returns it, so that it may be easily styled.
+var dom = function(el) {
+  log('dom: ' + el, 'blue');
+  let rv;
+
+  el = utils.queryDOM(el);
+
+  if (Array.isArray(el)) {
+    rv = [];
+    for (var l of el) {
+      rv.push(new Element(l))
+    }
+  } else {
+    rv = new Element(el);
+  }
+  return rv;
+};
+
+
+/*function for insterting template literals into the DOM.
+
+bones(`
+
+<html>
+  <head></head>
+  <body></body>
+</html>
+
+`);
+
+*/
+var __ = function(tempLit, el='body') {
+  return DOM.bones(tempLit, el);
+};
+
 
 
 //Create element alias function.
-var make = function(l, tag) {
-  return dom.make(l, tag);
+var make = function(el, tag) {
+  return DOM.make(el, tag);
 };
 
 
 //DOM querying alias function. Will automatically narrow class or tag queries down to one result. It will not return an array.
-var dom = function(l) {
-  return dom.dom(l);
+var el = function(l) {
+  return DOM.el(l);
 };
 
 
 //DOM querying function, same as above, but it will return an array if a tag or class name are given as argument.
-var query = function(l) {
-  return dom.query(l);
+var all = function(l) {
+  return DOM.all(l);
 };
 
 
 //Function for appending elements to other elements.
 var put = function(el, mom) {
-  return dom.put(el, mom);
+  return DOM.put(el, mom);
 };
 
 
 //Function for deleting elements from the DOM tree.
-var kill = function(el) {
-  return dom.kill(el);
+var x = function(el) {
+  return DOM.kill(el);
 };
 
 
 //console.log alias function.
-var log = (text, style, tyme)=> {
+var log = function(text, style, tyme) {
   return consol.log(text, style, tyme);
 };
 
 
 //console.error alias function.
-var err = (text, tyme)=> {
+var err = function(text, tyme) {
   return consol.err(text, tyme);
 };
 
 
 //console.info alias function.
-var info = (text, tyme)=> {
+var info = function(text, tyme) {
   return consol.info(text, tyme);
 };
 
 
 
 //console.warn alias function.
-var warn = (text, tyme)=> {
+var warn = function(text, tyme) {
   return consol.warn(text, tyme);
+};
+
+
+var shifter = function(onFunc, offFunc) {
+  return utils.shifter(onFunc, offFunc);
 };
 
 
 
 //This practically useless function will lock up the browser for a preset amount of time.
 var sleep = function(milliseconds) {
-  return misc.sleep(milliseconds);
+  return utils.sleep(milliseconds);
 };
 
 
@@ -131,9 +192,18 @@ var ajax = function(url, fd, callback, method) {
 /*---------Event Functions-----------*/
 
 
+var go = function(cb) {
+  if (document.addEventListener) {
+    return events.go(cb);
+  } else {
+    return events.goIE(cb);
+  }
+};
+
+
 //Function for setting event listeners.
 var on = function(event, el, callback) {
-  if (el.addEventListener) {
+  if (document.addEventListener) {
     return events.on(event, el, callback);
   } else {
     return events.onIE(event, el, callback);
@@ -143,7 +213,7 @@ var on = function(event, el, callback) {
 
 //Function for removing event listeners.
 var off = function(event, el, callback) {
-  if (el.addEventListener) {
+  if (document.addEventListener) {
     return events.off(event, el, callback);
   } else {
     return events.offIE(event, el, callback);
@@ -152,16 +222,29 @@ var off = function(event, el, callback) {
 
 
 var once = function(event, el, callback) {                                     //DONE:50 Finish once function.
-  if (el.addEventListener) {
+  if (document.addEventListener) {
     return events.once(event, el, callback);
   } else {
     return events.onceIE(event, el, callback);
-  }                                                                            //TEST:30 Test once function.
+  }                                                                            //TEST:70 Test once function.
+};
+
+
+var spark = function(evt, el) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
+    return events.spark(evt, el);
+  } else {
+    return events.sparkIE(evt, el);
+  }
 };
 
 
 var blur = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.blur(el, cb);
   } else {
     return events.blurIE(el, cb);
@@ -170,7 +253,9 @@ var blur = function(el, cb=null) {
 
 
 var click = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.click(el, cb);
   } else {
     return events.clickIE(el, cb);
@@ -179,7 +264,9 @@ var click = function(el, cb=null) {
 
 
 var dblClick = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.dblClick(el, cb);
   } else {
     return events.dblClickIE(el, cb);
@@ -188,7 +275,9 @@ var dblClick = function(el, cb=null) {
 
 
 var error = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.error(el, cb);
   } else {
     return events.errorIE(el, cb);
@@ -197,7 +286,9 @@ var error = function(el, cb=null) {
 
 
 var focus = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.focus(el, cb);
   } else {
     return events.focusIE(el, cb);
@@ -206,7 +297,9 @@ var focus = function(el, cb=null) {
 
 
 var focusIn = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.focusIn(el, cb);
   } else {
     return events.focusInIE(el, cb);
@@ -215,7 +308,9 @@ var focusIn = function(el, cb=null) {
 
 
 var focusOut = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.focusOut(el, cb);
   } else {
     return events.focusOut(el, cb);
@@ -224,7 +319,9 @@ var focusOut = function(el, cb=null) {
 
 
 var keyUp = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.keyUp(el, cb);
   } else {
     return events.keyUpIE(el, cb);
@@ -233,7 +330,9 @@ var keyUp = function(el, cb=null) {
 
 
 var keyDown = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.keyDown(el, cb);
   } else {
     return events.keyDownIE(el, cb);
@@ -242,7 +341,9 @@ var keyDown = function(el, cb=null) {
 
 
 var load = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.load(el, cb);
   } else {
     return events.loadIE(el, cb);
@@ -251,7 +352,9 @@ var load = function(el, cb=null) {
 
 
 var unLoad = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.unLoad(el, cb);
   } else {
     return events.unLoadIE(el, cb);
@@ -260,7 +363,9 @@ var unLoad = function(el, cb=null) {
 
 
 var mouse = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.mouse(el, cb);
   } else {
     return events.mouseIE(el, cb);
@@ -269,7 +374,9 @@ var mouse = function(el, cb=null) {
 
 
 var resize = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.resize(el, cb);
   } else {
     return events.resizeIE(el, cb);
@@ -278,7 +385,9 @@ var resize = function(el, cb=null) {
 
 
 var scroll = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.scroll(el, cb);
   } else {
     return events.scrollIE(el, cb);
@@ -287,7 +396,9 @@ var scroll = function(el, cb=null) {
 
 
 var select = function(el, cb=null) {
-  if (el.addEventListener) {
+  el = utils.queryDOM(el);
+
+  if (document.addEventListener) {
     return events.select(el, cb);
   } else {
     return events.selectIE(el, cb);
@@ -298,26 +409,29 @@ var select = function(el, cb=null) {
 
 
 
-
-
-
-
-
 module.exports = {
+               _$: _$,
+                x: x,
+               go: go,
+          shifter: shifter,
              make: make,
+               __: __,
               dom: dom,
             proto: proto,
         functions: functions,
-            query: query,
+              all: all,
               put: put,
-             kill: kill,
                on: on,
               off: off,
-              css: css,
-            style: style,
+             once: once,
+               el: el,
               log: log,
               xhr: xhr,
+              err: err,
+             info: info,
+             warn: warn,
              ajax: ajax,
+            spark: spark,
              blur: blur,
             click: click,
          dblClick: dblClick,
@@ -334,5 +448,6 @@ module.exports = {
            scroll: scroll,
            select: select,
                 };
+
 //DONE:30 functions: err(), info(), warn().
 //DONE:130 Complete all standalone style functions.
