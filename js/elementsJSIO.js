75081b3bbbf644d2da8ace2cde2d20e6e1d964cd
ejames9 <foster9@icloud.com>
2016-03-28 20:38:31 -0700

    Added npm/bower install bar.
    Includes associated changes in index.html and elementsJSIO.css.  Created elementsJSIO.js file.

---
+++ js/elementsJSIO.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+function toggleNPMBar() {
+  var npmBar = document.getElementById('npm-bar');
+
+      if (npmBar.style.display === 'none') {
+        npmBar.style.display = 'block';
+      } else {
+        npmBar.style.display = 'none';
+      }
+};
+
+
+function init() {
+  var npmBar = document.getElementById('npm-bar');
+      npmBar.style.display = 'none';
+
+  var html = document.getElementsByTagName('html')[0];
+      html.addEventListener('click', function(e) {
+        
+        if (e.target === document.getElementById('install-info')) {
+          console.log("we're in");
+          toggleNPMBar();
+        }
+      });
+};
+
+
+
+document.addEventListener('DOMContentLoaded', init());
