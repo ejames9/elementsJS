90e31ee57d9fd59db5ea85b137f55d01249dc54b
ejames9 <foster9@icloud.com>
2016-04-10 21:52:15 -0700

    .

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,39 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
 var run = require('run-sequence');
+var gulpWebpack = require('gulp-webpack');
 
 
+const webpackConfig = {
+  entry: './dist/babel/elementsJSIO.js',
+  output: {
+    filename: './dist/elementsJSIO.js'
+  }
+};
+
+gulp.task('elemsJSIO-Babel', ()=> {
+  var srcJS = './js/elementsJSIO.js';
+  var jsDst  = './dist/babel/';
+
+  return gulp.src(srcJS)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest(jsDst));
+});
+
+gulp.task('elemsJSIO-Bundle', ()=> {
+  var compiler = gulpWebpack(webpackConfig, webpack),
+         jsSrc = './dist/babel/elementsJSIO.js',
+         jsDst = './dist/'
+  return gulp.src(jsSrc)
+    .pipe(compiler)
+    .pipe(gulp.dest(jsDst));
+});
+
+gulp.task('elemsJSIO', ()=> {
+  return run('elemsJSIO-Babel', 'elemsJSIO-Bundle');
+});
 
 
 gulp.task('dist1', ()=> {
