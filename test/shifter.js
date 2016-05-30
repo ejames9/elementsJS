0f5367f8cc397c00ed92e90ee355f00dfedc36f0
ejames9 <foster9@icloud.com>
2016-05-30 12:40:08 -0700

    .

--- test/shifter.js
+++ test/shifter.js
@@ -8,13 +8,35 @@ document.addEventListener('DOMContentLoaded', function() {
   logo.addEventListener('click', function() {
     _404_.style.display = 'none';
     moka.style.visibility = 'visible';
-    
-    TEST_FLAG = true;
+
+     document.cookie = 'TESTFLAG=true';
 
     // mocha.run();
   });
+  var
+  cookie = document.cookie;
+  console.log(cookie);
+  console.log(getCookie(
+    'TESTFLAG'
+  ));
 
-  if (TEST_FLAG) {
+  if (getCookie('TESTFLAG')) {
     moka.style.visibility = 'visible';
   }
 });
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
