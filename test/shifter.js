05510ef32cdf7f1b22fdd26d71408e9f0ee33d8e
ejames9 <foster9@icloud.com>
2016-05-30 01:22:48 -0700

    .

---
+++ test/shifter.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  var
+  moka = document.getElementById('mocha'),
+  logo  = document.getElementById('logo'),
+  _404_ = document.getElementById('_404_content');
+
+  logo.addEventListener('click', function() {
+    _404_.style.display = 'none';
+    moka.style.display = 'block';
+
+    mochaPhantomJS.run();
+  });
+});
